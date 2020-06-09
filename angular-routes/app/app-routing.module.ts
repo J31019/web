e064f8a3a0faee1a2ff62d3b9e0084275b14727f
Artem Gutorov <gutorov.artem@yandex.ru>
2020-06-09 21:17:25 +0300

    Add files via upload

---
+++ angular-routes/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/c2",
+    pathMatch: "full"
+  },
+  {
+    path: "c1",
+    component: Comp1Component
+  },
+  {
+    path: "c2",
+    component: Comp2Component
+  },
+  {
+    path: "**", // ** - путь не найден
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
